3a52ed4584afd017046a15167b48fb3612ef6e5a
MariaFernanda1818 <83258884+MariaFernanda1818@users.noreply.github.com>
2025-06-04 17:33:07 -0500

    New endpoints, services and data are being added.

--- src/main/java/co/edu/uni/acme/airline/fee/controller/ServicePassengerController.java
+++ src/main/java/co/edu/uni/acme/airline/fee/controller/ServicePassengerController.java
@@ -1,13 +1,11 @@
 package co.edu.uni.acme.airline.fee.controller;
 
+import co.edu.uni.acme.airline.fee.dto.RespuestaDto;
 import co.edu.uni.acme.airline.fee.dto.ServicePassengerRequestDto;
 import co.edu.uni.acme.airline.fee.service.ICreateServicePassengerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/service-passenger")
@@ -17,9 +15,11 @@ public class ServicePassengerController {
     private final ICreateServicePassengerService iCreateServicePassengerService;
 
     @PostMapping("create")
-    public ResponseEntity<String> createServicePassenger(@RequestBody ServicePassengerRequestDto request) {
+    public ResponseEntity<RespuestaDto> createServicePassenger(@RequestBody ServicePassengerRequestDto request) {
         boolean isCreate = iCreateServicePassengerService.createServicePassenger(request);
-        return ResponseEntity.status(isCreate ? 200 : 400).body(isCreate ? "Se creo correctamente el passenger" : "No se creo el passager");
+        RespuestaDto respuestaDto = new RespuestaDto();
+        respuestaDto.setMessage(isCreate ? "Se creo correctamente el passenger" : "No se creo el passager");
+        return ResponseEntity.status(isCreate ? 200 : 400).body(respuestaDto);
     }
 
 }
---
+++ src/main/java/co/edu/uni/acme/airline/fee/dto/RespuestaDto.java
@@ -0,0 +1,10 @@
+package co.edu.uni.acme.airline.fee.dto;
+
+import lombok.Data;
+
+@Data
+public class RespuestaDto {
+
+    private String message;
+
+}
